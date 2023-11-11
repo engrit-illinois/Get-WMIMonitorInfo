7e816403b2140f023e9e630c2c5b4e325849cd11
James Han <han44@illinois.edu>
2023-11-11 00:12:03 -0600

    Added Diagonal Size and Ratio to the output.
    This required utilizing another WMI Class, so the `JoinModule` is imported to facilitate joining the two classes for this query.

--- Get-WMIMonitorInfo.psm1
+++ Get-WMIMonitorInfo.psm1
@@ -23,21 +23,55 @@ function Get-Manufacturer {
     $output
 }
 
+function Measure-Diagonal {
+    param (
+        $Monitor
+    )
+
+    $Horizontal =   $Monitor | Select-Object -ExpandProperty MaxHorizontalImageSize
+    $Vertical =     $Monitor | Select-Object -ExpandProperty MaxVerticalImageSize
+
+    # Convert to Inches from CM
+    $Horizontal =   [System.Math]::Round(($Horizontal/2.54),2)
+    $Vertical =     [System.Math]::Round(($Vertical/2.54),2)
+
+    # Pythagorean Theorem rounded to the nearest inch
+    $Diagonal = [System.Math]::Round([System.Math]::Sqrt([System.Math]::Pow($Horizontal,2) + [System.Math]::Pow($Vertical,2)),0)
+    $Diagonal
+}
+
+function Measure-Ratio {
+    param(
+        $Monitor
+    )
+
+    $Horizontal =   $Monitor | Select-Object -ExpandProperty MaxHorizontalImageSize
+    $Vertical =     $Monitor | Select-Object -ExpandProperty MaxVerticalImageSize
+
+    # Convert to Inches from CM
+    $Horizontal =   [System.Math]::Round(($Horizontal/2.54),2)
+    $Vertical =     [System.Math]::Round(($Vertical/2.54),2)
+
+    $Ratio = [System.Math]::Round(($Horizontal/$Vertical),2)
+    $Ratio
+}
+
 function Build-ArrayObject {
     param(
-        $WMIMonitorID,
-        $WmiMonitorBasicDisplayParams
+        $Monitor
     )
     $output = New-Object System.Collections.ArrayList
     
     $output = [PSCustomObject]@{
-        Manufacturer = Get-Manufacturer -Monitor $WMIMonitorID
-        ProductCode = Decode $WMIMonitorID.ProductCodeID
-        Serial = Decode $WMIMonitorID.SerialNumberID
-        Name = Decode $WMIMonitorID.UserFriendlyName
-        WeekOfManufacture = $WMIMonitorID.WeekOfManufacture
-        YearOfManufacture = $WMIMonitorID.YearOfManufacture
-        PSComputerName = $WMIMonitorID.PSComputerName
+        Manufacturer = Get-Manufacturer -Monitor $Monitor
+        ProductCode = Decode $Monitor.ProductCodeID
+        Serial = Decode $Monitor.SerialNumberID
+        Name = Decode $Monitor.UserFriendlyName
+        WeekOfManufacture = $Monitor.WeekOfManufacture
+        YearOfManufacture = $Monitor.YearOfManufacture
+        Size = Measure-Diagonal -Monitor $Monitor
+        Ratio = Measure-Ratio -Monitor $Monitor
+        PSComputerName = $Monitor.PSComputerName
     }
     Write-Verbose $output
     $output
@@ -51,6 +85,11 @@ function Get-WMIMonitorInfo {
         [Parameter(ParameterSetName="LocalSet")]
         [switch]$Local
     )
+
+    if(-not (Get-Module JoinModule)){
+        Install-Module JoinModule
+    }
+
     $output = New-Object System.Collections.ArrayList
 
     if($ComputerName){
@@ -59,14 +98,18 @@ function Get-WMIMonitorInfo {
         }else{
             throw "Could not ping remote computer."
         }
-        $WMIMonitorID = Get-CimInstance -ClassName WMIMonitorID -Namespace root\wmi -ComputerName $ComputerName
+        $WMIMonitorID = Get-CimInstance -Namespace root\wmi -ClassName WMIMonitorID -ComputerName $ComputerName
+        $WmiMonitorBasicDisplayParams = Get-Ciminstance -Namespace root\wmi -ClassName WmiMonitorBasicDisplayParams -ComputerName $ComputerName
     }elseif($Local){
         $WMIMonitorID = Get-CimInstance -ClassName WMIMonitorID -Namespace root\wmi
+        $WmiMonitorBasicDisplayParams = Get-Ciminstance -Namespace root\wmi -ClassName WmiMonitorBasicDisplayParams
     }
 
-    foreach($Monitor in $WMIMonitorID) {
+    $Combined = $WMIMonitorID | Join-Object $WmiMonitorBasicDisplayParams -On InstanceName,PSComputerName
+
+    foreach($Monitor in $Combined) {
         Write-Verbose $Monitor
-        $Member = Build-ArrayObject -WMIMonitorID $Monitor
+        $Member = Build-ArrayObject -Monitor $Monitor
         $output.Add($Member) | Out-Null
     }
     $output
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Mostly made this for convenience, and also because examples on the internet either used the dated `Get-WMIObject` cmdlet, didn't output the results as a PowerShell object, or were lost due to being hosted on Microsoft TechNet. Hate that I'm basically reinventing a wheel, but it is what it is.
 
+This module makes use of `JoinModule`, the source for which can be found here: https://github.com/iRon7/Join-Object
+
 ## Examples
 
 Queries and returns the monitor info from target computer `EH-406B8-01` 
