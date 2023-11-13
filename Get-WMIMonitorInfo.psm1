44b99be3b4bdbbc1e546b508b3e1c4a37f3b6300
James Han <han44@illinois.edu>
2023-11-13 11:55:06 -0600

    Made common ratios human-readable

--- Get-WMIMonitorInfo.psm1
+++ Get-WMIMonitorInfo.psm1
@@ -53,7 +53,20 @@ function Measure-Ratio {
     $Vertical =     [System.Math]::Round(($Vertical/2.54),2)
 
     $Ratio = [System.Math]::Round(($Horizontal/$Vertical),2)
-    $Ratio
+    switch($Ratio){
+        {$_ -ge 1.75 -and $_ -le 1.80} {$Numerator = 16; $Denominator = 9}
+        {$_ -ge 1.57 -and $_ -le 1.63} {$Numerator = 16; $Denominator = 10}
+        {$_ -ge 1.20 -and $_ -le 1.40} {$Numerator = 4; $Denominator = 3}
+    }
+
+    if($Numerator -and $Denominator){
+        $output = "$($Numerator):$($Denominator)"
+    }
+    if($output){
+        $output
+    }else{
+        $Ratio
+    }
 }
 
 function Build-ArrayObject {
