bfd65fb41d3b6114de14b6d85202b96670417153
James Han <han44@illinois.edu>
2023-11-29 16:16:45 -0600

    Update Get-WMIMonitorInfo.psm1

--- Get-WMIMonitorInfo.psm1
+++ Get-WMIMonitorInfo.psm1
@@ -79,25 +79,26 @@ function Get-VideoOutputTechnology {
     $output = $Monitor.VideoOutputTechnology
 
     switch($output) {
-        -2  {"Unassigned"}
-        -1  {"Unknown"}
-        0   {"VGA"}
-        1   {"S-vidio"}
-        2   {"Composite Video"}
-        3   {"Component Video"}
-        4   {"DVI"}
-        5   {"HDMI"}
-        6   {"LVDS"}
-        8   {"D-Jpn"}
-        9   {"SDI"}
-        10  {"External DisplayPort"}
-        11  {"Embedded DisplayPort"}
-        12  {"External UDI"}
-        13  {"Embedded UDI"}
-        14  {"SDTV"}
-        15  {"Miracast"}
-        16  {"Wired Indirect Display"}
-        Default {$output}
+        -2          {"Unassigned"}
+        -1          {"Unknown"}
+        0           {"VGA"}
+        1           {"S-vidio"}
+        2           {"Composite Video"}
+        3           {"Component Video"}
+        4           {"DVI"}
+        5           {"HDMI"}
+        6           {"LVDS"}
+        8           {"D-Jpn"}
+        9           {"SDI"}
+        10          {"External DisplayPort"}
+        11          {"Embedded DisplayPort"}
+        12          {"External UDI"}
+        13          {"Embedded UDI"}
+        14          {"SDTV"}
+        15          {"Miracast"}
+        16          {"Wired Indirect Display"}
+        2147483648  {"Internal Laptop Display"}
+        Default     {$output}
     }
     
 }
